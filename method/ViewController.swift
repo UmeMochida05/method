19be9c4444c9724a7f114175fded60d8da48bdd1
YouriMochida <you05.smie@gmail.com>
2023-06-22 21:41:01 +0900

    Initial Commit

---
+++ method/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  method
+//
+//  Created by 持田ゆうり on 2023/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var number1textfield: UITextField!
+    @IBOutlet var number2textfield: UITextField!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var button: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if let number1text = number1textfield.text, let number2text = number2textfield.text, let number1 = Int(number1text), let number2 = Int(number2text) {
+            let sum = number1 + number2
+            label.text = String(sum)
+        } else {
+            return
+        }
+    }
+
+
+}
+
